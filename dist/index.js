e22d0b1b35aa9b9d83a83fa1e8fe7618058be0b1
Joe Pea <joe@trusktr.io>
2024-10-02 14:08:55 -0700

    v0.1.5

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 export * from './CodeMirror.js';
 export * from './CodeMirrorContentchangedEvent.js';
-export const version = '0.1.4';
+export const version = '0.1.5';
 //# sourceMappingURL=index.js.map
