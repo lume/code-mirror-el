378573603a04342510256b8d90458c6a190b1c9e
Joe Pea <joe@trusktr.io>
2024-10-10 15:31:25 -0700

    v0.2.1

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
 export * from './CodeMirror.js';
 export * from './CodeMirrorContentchangedEvent.js';
-export declare const version = "0.2.0";
+export declare const version = "0.2.1";
 //# sourceMappingURL=index.d.ts.map
